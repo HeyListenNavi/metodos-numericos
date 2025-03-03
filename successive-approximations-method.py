8226fe49d8c3cacbd74c1ce2ea499d249ccd3b9b
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2025-03-02 16:20:26 -0800

    Refactor error calculation in successive approximations method

--- successive-approximations-method.py
+++ successive-approximations-method.py
@@ -3,15 +3,15 @@ from typing import Callable, Optional
 
 def successive_approximations_recursive(f: Callable[[float], float], tol: float, x: float = 0.5, iteration: int = 1) -> float:
     new_x: float = f(x) 
-    difference: float = abs(new_x - x)
+    error: float = abs((new_x - x) / new_x)
 
     # Print iteration in table format
     print(f"{'Iteration':<10}{'x':<10}{'g(x)':<10}{'Dif':<10}")
-    print(f"{iteration:<10}{x:<10.6f}{new_x:<10.6f}{difference:<10.2E}")
+    print(f"{iteration:<10}{x:<10.6f}{new_x:<10.6f}{error:<10.2E}")
     print()
     
     # Check tolerance
-    isDifferenceLessThanTolerance: bool = difference < tol
+    isDifferenceLessThanTolerance: bool = error < tol
 
     if isDifferenceLessThanTolerance:
         return new_x
